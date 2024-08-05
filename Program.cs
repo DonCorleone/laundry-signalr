6ba1c2b669c46dff4640d1658fd9bcea2c02ebc9
Linus Wieland <linus@wieland.click>
2024-08-06 00:11:03 +0200

    no redis

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 
-using System.Net;
 using LaundrySignalR.Hubs;
-using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
@@ -15,35 +13,8 @@ builder.Services.AddCors(options =>
                 .AllowCredentials();
         });
 });
-builder.Services
-    .AddSignalR()
-    .AddStackExchangeRedis(
-        "redis://red-cqnqmfg8fa8c73at4hog:6379",
-        o =>
-        {
-            o.ConnectionFactory = async writer =>
-            {
-                var config = new ConfigurationOptions
-                {
-                    AbortOnConnectFail = false
-                };
-                config.EndPoints.Add(IPAddress.Loopback, 0);
-                config.SetDefaultPorts();
-                var connection = await ConnectionMultiplexer.ConnectAsync(config, writer);
-                connection.ConnectionFailed += (_, e) =>
-                {
-                    Console.WriteLine("Connection to Redis failed.");
-                };
+builder.Services.AddSignalR();
 
-                if (!connection.IsConnected)
-                {
-                    Console.WriteLine("Did not connect to Redis.");
-                }
-
-                return connection;
-            };
-            o.Configuration.AbortOnConnectFail = false;
-        });
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
