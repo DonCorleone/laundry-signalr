2ae40d97f4d16265d23754d9424dfef3463a8557
Linus Wieland <linus@wieland.click>
2024-08-15 22:21:43 +0200

    running laundry redis

--- Controllers/ReservationEntriesController.cs
+++ Controllers/ReservationEntriesController.cs
@@ -24,36 +24,24 @@ public class ReservationEntriesController(
     public async Task<IActionResult> Post([FromBody] ReservationEntry reservationEntry)
     {
         // Here you can add code to save the reservationEntry to a database if needed
-        Add(reservationEntry);
+        bool added = Add(reservationEntry);
+        
+        if (!added)
+        {
+            return BadRequest();
+        }
+        
         // Notify clients via SignalR
         await hubContext.Clients.All.ReservationAdded(reservationEntry);
 
         return Ok(reservationEntry);
     }
 
-    private void Add(ReservationEntry reservationEntry)
+    private bool Add(ReservationEntry reservationEntry)
     {
-        bool res1 = _db.SortedSetAdd("racer_scores", "Norem", 10);
-        Console.WriteLine(res1); // >>> True
-
-        bool res2 = _db.SortedSetAdd("racer_scores", "Castilla", 12);
-        Console.WriteLine(res2); // >>> True
-
-        long res3 = _db.SortedSetAdd("racer_scores", new[]{
-            new SortedSetEntry("Sam-Bodden", 8),
-            new SortedSetEntry("Royce", 10),
-            new SortedSetEntry("Ford", 6),
-            new SortedSetEntry("Prickett", 14),
-            new SortedSetEntry("Castilla", 12)
-        });
-        Console.WriteLine(res3); // >>> 4
-        
-        RedisValue[] res4 = _db.SortedSetRangeByRank("racer_scores", 0, -1);
-        Console.WriteLine(string.Join(", ", res4)); // >>> Ford, Sam-Bodden, Norem, Royce, Castilla, Prickett
-
-        RedisValue[] res5 = _db.SortedSetRangeByRank("racer_scores", 0, -1, Order.Descending);
-        Console.WriteLine(string.Join(", ", res5)); // >>> Prickett, Castilla, Royce, Norem, Sam-Bodden, Ford
-
+        bool res = _db.SortedSetAdd(reservationEntry.Tags[0], reservationEntry.Name, reservationEntry.Id);
+        Console.WriteLine(res); 
+        return res;
     }
 
     [HttpDelete]
